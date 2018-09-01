ab86690edd7947c350cbbd1d54ec1bc9f0b7ac24
We <lbabasyuk@gmail.com>
2018-09-01 00:37:58 -0400

    creating new contact_final

--- contact.py
+++ contact.py
@@ -1,5 +1,5 @@
 class Contact:
-    def __init__(self, firstname, middlename, lastname, nickname, photo, title, company, address, home_phonenumber, mobile_number, work_number, fax_number, email1, email2, email3, website, Byear, ayear, secondary_address, secondary_phone, secondary_notes):
+    def __init__(self, firstname, middlename, lastname, nickname, photo, title, company, address, homenumber, mobile, worknumber, fax, email, website, byear, ayear, address2, phone2, notes):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -8,16 +8,14 @@ class Contact:
         self.title = title
         self.company = company
         self.address = address
-        self.home_phonenumber = home_phonenumber
-        self.mobile_number = mobile_number
-        self.work_number = work_number
-        self.fax_number = fax_number
-        self.email1 = email1
-        self.email2 = email2
-        self.email3 = email3
+        self.homenumber = homenumber
+        self.mobile = mobile
+        self.worknumber = worknumber
+        self.fax = fax
+        self.email = email
         self.website = website
-        self.Byear = Byear
+        self.byear = byear
         self.ayear = ayear
-        self.secondary_address = secondary_address
-        self.secondary_phone = secondary_phone
-        self.secondary_notes = secondary_notes
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
