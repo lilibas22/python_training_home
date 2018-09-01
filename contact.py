a28ceb722f57022a5a1eaffa83ad2925272c35a8
We <lbabasyuk@gmail.com>
2018-08-31 21:39:57 -0400

    creating new contact

---
+++ contact.py
@@ -0,0 +1,23 @@
+class Contact:
+    def __init__(self, firstname, middlename, lastname, nickname, photo, title, company, address, home_phonenumber, mobile_number, work_number, fax_number, email1, email2, email3, website, Byear, ayear, secondary_address, secondary_phone, secondary_notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.photo = photo
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_phonenumber = home_phonenumber
+        self.mobile_number = mobile_number
+        self.work_number = work_number
+        self.fax_number = fax_number
+        self.email1 = email1
+        self.email2 = email2
+        self.email3 = email3
+        self.website = website
+        self.Byear = Byear
+        self.ayear = ayear
+        self.secondary_address = secondary_address
+        self.secondary_phone = secondary_phone
+        self.secondary_notes = secondary_notes
