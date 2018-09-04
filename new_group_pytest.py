b9951e3b836f5ec0268ad3c1e96e9aa0b0c91440
We <lbabasyuk@gmail.com>
2018-09-04 17:33:56 -0400

    from unittest to pytest and fixture realization

---
+++ new_group_pytest.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from application import Application
+import pytest
+from group import Group
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="Home_Liliia", header="header", footer="footer"))
+    app.logout()
+
+
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
