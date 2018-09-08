ec19471dd1e8061b54c4f3c4859573de8409ac5a
We <lbabasyuk@gmail.com>
2018-09-08 13:20:18 -0400

    highlighted class for group managing

--- test/new_group_pytest.py
+++ test/new_group_pytest.py
@@ -12,11 +12,11 @@ def app(request):
 
 def test_add_group(app):
     app.login(username="admin", password="secret")
-    app.create_group(Group(name="Home_Liliia", header="header", footer="footer"))
+    app.create(Group(name="Home_Liliia", header="header", footer="footer"))
     app.logout()
 
 
 def test_add_empty_group(app):
     app.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.create(Group(name="", header="", footer=""))
     app.logout()
