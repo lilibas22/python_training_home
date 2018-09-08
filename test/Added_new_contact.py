34b2b953bf5d515a808798948dca5d2db54abec7
We <lbabasyuk@gmail.com>
2018-09-08 14:31:23 -0400

    highlighted class for contact managing

--- test/Added_new_contact.py
+++ test/Added_new_contact.py
@@ -11,8 +11,8 @@ def app(request):
 
     
 def test_Added_new_contact(app):
-    app.login(username="admin", password="secret")
-    app.new_contact(Contact(firstname="Brad", middlename="BP", lastname="Pitt", nickname="Braddy", photo="C:\\12.jpg", title="Junior", company="21 Fox", address="Hollywood", homenumber="123456789",
+    app.session.login(username="admin", password="secret")
+    app.contact.new_contact(Contact(firstname="Brad", middlename="BP", lastname="Pitt", nickname="Braddy", photo="C:\\12.jpg", title="Junior", company="21 Fox", address="Hollywood", homenumber="123456789",
                          mobile="987654321", worknumber="678901234", fax="123123123", website="www.pitt.com", email="test1@test.com", byear="1989", ayear="1900",
                          address2="Miami", phone2="785269555", notes="Notes here"))
-    app.logout()
+    app.session.logout()
