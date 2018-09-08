faa68ed46b5ac489386e6a8b090984a102356763
We <lbabasyuk@gmail.com>
2018-09-08 12:51:16 -0400

    highlighted class for sessions managing

--- test/Python_home_Liliia.py
+++ test/Python_home_Liliia.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from fixture.application import Application
 import pytest
+
 from model.group import Group
 
 @pytest.fixture
@@ -11,12 +12,12 @@ def app(request):
 
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="Home_Liliia", header="header", footer="footer"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
