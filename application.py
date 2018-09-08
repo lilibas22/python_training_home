9aeae771562e272d7a2ab7a3804a457a729068c0
We <lbabasyuk@gmail.com>
2018-09-08 10:45:02 -0400

    fixture realization for contact test

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ from selenium.webdriver.firefox.webdriver import WebDriver
 class Application:
 
     def __init__(self):
-           self.wd = WebDriver(capabilities={"marionette": False})
+           self.wd = WebDriver(firefox_binary="C:/Program Files/Mozilla Firefox/firefox.exe")
            self.wd.implicitly_wait(60)
 
     def open_home_page(self):
